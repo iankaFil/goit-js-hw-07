f5076c729f266ce4075f27e11e80147f1f7b337d
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-11-09 13:30:24 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,31 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(".gallery");
 
-// function createGallery({ preview, original, description }) {
-//     return `<div class="gallery__item">
-//     <a class="gallery__link" href="${original}">
-//       <img
-//         class="gallery__image"
-//         src="${preview}"
-//         data-source="${original}"
-//         alt="${description}"
-//       />
-//     </a>
-//   </div>`;
-// };
-// const galleryItem = galleryItems.map(createGallery).join("");
-
-// gallery.innerHTML += galleryItem;
-
-// gallery.addEventListener("click", openImage)
-
-// function openImage(event) {
-//     event.preventDefault();
-
-//     if (event.target.nodeName !== "IMG")
-//         return;
-// }
-
 function makeGallery(arr) {
     const galleryItemsMarkup = arr.map(({ preview, original, description }) => {
         return `<li><a class="gallery__item" 
